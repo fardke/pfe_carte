c4f80e96d45749b82175a7126f286ec310d99164
Kévin Fardel <kevin.fardel@gmail.com>
2011-01-15 16:58:36 +0100

    Correction de certaines erreur du coté serveur.

--- main/synchroServeur.c
+++ main/synchroServeur.c
@@ -1,7 +1,7 @@
 #include "bt_p.h"
 
 int main(void){
-	char* addr;
+	char addr[256];
 	printf("Adresse de la carte avec qui nous allons faire la synchronisation: ");
 	scanf("%s",addr);
 	synchroServeur(addr);
